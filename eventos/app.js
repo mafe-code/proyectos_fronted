7ed431378e242bfec99fd98f16e61c00236eeafa
mafe-code <correamafe591@gmail.com>
2026-05-08 21:57:15 -0500

    proyecto

---
+++ eventos/app.js
@@ -0,0 +1,28 @@
+
+const boton=document.querySelector('button')
+/*
+boton.addEventListener('click',saludar);
+
+function saludar() {
+    console.log("Hola mundo")
+}
+
+
+boton.onclick= function saludar() {
+    console.log("Hola mundo")
+}
+    
+boton.addEventListener('mouseover',function() {
+    boton.style.backgroundColor='verde';
+});
+
+boton.removeEventListener('mouseover',function(){
+    boton.style.backgroundColor='red';
+})
+
+*/
+
+boton.addEventListener('dblclick',event);
+function event() {
+    console.log("event")
+}
